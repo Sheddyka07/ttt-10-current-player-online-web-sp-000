4b89cc0ff4506ef006e6822a343cdc5aea7bcf1d
Kelly Sheddy <jungaka07@gmail.com>
2018-11-08 14:30:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,15 +3,5 @@ def turn_count(board)
   puts turn_amount
 end
   
-def current_player(board)
-  num_xs = board.count("X")
-  num_os = board.count("O")
-  if board.empty? 
-    puts "X"
-  elsif num_xs % 2 == 0 && num_os % 2 == 0 
-    puts "X"
-  else
-    puts "O"
-  end 
-end
+
   
