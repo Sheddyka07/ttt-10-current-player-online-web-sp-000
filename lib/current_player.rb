96133c8ebb6443da63ebccca69392455920de6ba
Kelly Sheddy <jungaka07@gmail.com>
2018-11-08 14:22:52 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,7 @@ def turn_count(board)
   num_xs = board.count("X")
   num_os = board.count("O")
   
-  if num_xs % 2 == 0 && num_os % 2 == 0 
+  if board 
+  elseif num_xs % 2 == 0 && num_os % 2 == 0 
     puts "X"
   elsif num_os % 2 == 0
